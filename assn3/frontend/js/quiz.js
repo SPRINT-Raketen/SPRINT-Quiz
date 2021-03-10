5d447488b92cf4bee9e860bacfc43dd6e4cd5ba4
Widmayer Jan <69035907+janwidmayer@users.noreply.github.com>
2021-03-10 15:16:40 +0100

    Update quiz.js

--- assn3/frontend/js/quiz.js
+++ assn3/frontend/js/quiz.js
@@ -3,29 +3,36 @@ $(function(){
 });
 
 function getRightAnswer(){
-    return currentQuestion.right;
+    var correctAnswer = fragen[pos][6];
+    return correctAnswer;
 }
 
-var currentQuestionNo = 0;
+/*var currentQuestionNo = 0;
 var value =0;
-var currentQuestion;
+var currentQuestion;*/
 
 
-let fragen = [['1', 'Was macht man mit einer Programmiersprache?', 'Wandern', 'Kochen', 'Programmieren', 'Zeichnen'],
-  ['2', 'Wie nennt man einen Fehler in einem Computerprogramm?','Bug', 'Hat', 'Pen','Code']];
-var pos = 0;
+let fragen = [['1', 'Was macht man mit einer Programmiersprache?', 'Wandern', 'Kochen', 'Programmieren', 'Zeichnen', 'C'],
+  ['2','Wie nennt man einen Fehler in einem Computerprogramm?','Bug', 'Hat', 'Pen','Code', 'A'],
+  ['3','Wie nennt man ein Textverarbeitungsprogramm?','Word', 'Works', 'Murks','Knorz', 'A']];
+var pos = -1;
 
 function showQuestion() {
+  pos+=1;
   $("#qno").text(fragen[pos][0]);
   $("#question_text").text(fragen[pos][1]);
   $("#answer_a").text(fragen[pos][2]);
   $("#answer_b").text(fragen[pos][3]);
   $("#answer_c").text(fragen[pos][4]);
   $("#answer_d").text(fragen[pos][5]);
-  pos+=1;
 }
 
-function checkAnswer() {
-    alert("your answer is correct " + value);
-
+function checkAnswer(radio) { 
+  if (radio.value == getRightAnswer()) {
+   alert("Gratulation, your answer is correct " + radio.value);
+   showQuestion();  
+ } else {
+   alert("Sorry, your answer is not correct: " + radio.value);
+ }
+ $('input:checked').removeAttr('checked');
 }
